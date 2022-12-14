7574df5ed7f1c71ad981c499c09a5d5e6c78ae8b
Your NameEmmanuel <agbavweisaac@gmail.com>
2022-12-14 12:48:31 +0100

    Update first test case passes :)

--- index.test.mjs
+++ index.test.mjs
@@ -2,8 +2,6 @@ import { test } from '@reach-sh/stdlib'
 import * as backend from './build/index.main.mjs'
 import {
 	reach,
-	fmt,
-	b2N,
 	setup,
 	params,
 	logView,
@@ -18,31 +16,40 @@ test.one('Users can stake, claim, and unstake', async () => {
 	const { admin, creator, user, testAccounts, stakeToken, rewardToken } =
 		await setup()
 	const ctc = deploy(creator)
-	ctc.p.Creator({
-		getParams: async () => await params(admin, stakeToken, rewardToken),
-		deployed: async () => {
-			const info = await ctc.getInfo()
-			await runAccts(ctc, info, testAccounts, stakeToken, rewardToken)
-			await claimFees(admin, ctc, info, stakeToken, rewardToken)
-			await logBalances(
-				users,
-				[admin, creator, user, ...testAccounts],
-				stakeToken,
-				rewardToken
-			)
-			const global = await ctc.v.global()
-			logView('global', global)
-		},
-	})
 
-	const info = await ctc.getInfo()
+	const info = ctc.getInfo()
 	const ctcUser = user.contract(backend, info)
 	console.log('[+] attached user to the main contract')
-	ctcUser.p.User({
-		deployed: () => {
-			console.log('[+] user saw deploy confirmed')
-		},
-	})
+
+	return await Promise.all([
+		reach.withDisconnect(() =>
+			ctc.p.Creator({
+				getParams: async () => await params(admin, stakeToken, rewardToken),
+				deployed: async () => {
+					const info = await ctc.getInfo()
+					await runAccts(ctc, info, testAccounts, stakeToken, rewardToken)
+					await claimFees(admin, ctc, info, stakeToken, rewardToken)
+					await logBalances(
+						users,
+						[admin, creator, user, ...testAccounts],
+						stakeToken,
+						rewardToken
+					)
+					const global = await ctc.v.global()
+					logView('global', global)
+					reach.disconnect(null)
+				},
+			})
+		),
+		reach.withDisconnect(() =>
+			ctcUser.p.User({
+				deployed: () => {
+					console.log('[+] user saw deploy confirmed')
+					reach.disconnect(null)
+				},
+			})
+		),
+	])
 })
 
 await test.run({ noVarOutput: true })
