20e731ea1d370faad09f8082a8edc134f5d71f81
Your NameEmmanuel <agbavweisaac@gmail.com>
2022-12-15 04:19:59 +0100

    Update cometa contract test suite complete

--- index.func.2.js
+++ index.func.2.js
@@ -1,14 +1,189 @@
+import { test } from '@reach-sh/stdlib'
+import { reach, fmt, b2N, logView, logBalances, tAcc } from './index.func.0.js'
+import * as backend from './build/index.main.mjs'
 
+export const setup = async () => {
+	const adminStartingBalance = reach.parseCurrency(100)
+	const creatorStartingBalance = reach.parseCurrency(2_000_000)
+	const startingBalance = reach.parseCurrency(100)
 
+	const admin = await reach.newTestAccount(adminStartingBalance)
+	admin.setDebugLabel('admin')
+	console.log('[+] admin account created')
 
+	const creator = await reach.newTestAccount(creatorStartingBalance)
+	creator.setDebugLabel('creator')
+	console.log('[+] creator account created')
 
+	const user = await reach.newTestAccount(startingBalance)
+	user.setDebugLabel('user')
+	console.log('[+] user account created')
 
-export const params = async (admin, stakeToken, rewardToken) => ({
+	const opts = {
+		decimals: 6,
+		supply: 1_000_000_000,
+	}
+
+	const token1 = await reach.launchToken(admin, 'Aro1914', 'A19', opts)
+	const rewardToken = token1.id
+	console.log(
+		'[+] admin minted the reward token: Aro1914, supply: 1,000,000,000'
+	) // token earned by users overtime for thier stake
+
+	const token2 = await reach.launchToken(admin, 'Lonewolf1914', 'LONE19', opts)
+	const stakeToken = token2.id
+	console.log(
+		'[+] admin minted the stake token: Lonewolf1914, supply: 1,000,000,000'
+	) // token to be staked by users, usually LP tokens, but in this case could be DAO tokens
+
+	await creator.tokenAccept(stakeToken)
+	await creator.tokenAccept(rewardToken)
+	await reach.transfer(admin, creator, 2_000_000, rewardToken)
+	console.log('[+] transferred 2,000,000 Aro1914 to creator') // this is to enable the creator send in some reward tokens to be distributed to users in rewards
+
+	return {
+		admin,
+		creator,
+		user,
+		stakeToken,
+		rewardToken,
+	}
+}
+
+export const params_1 = async (admin, stakeToken, rewardToken) => ({
 	beneficiary: admin.getAddress(),
 	creationFee: 10, // 0.1%,
 	// 1. attracts a 1,000 Algo creationAlgoFeeToPay from the totalAlgoRewardAmount of 1,000,000 Algos
-	// 2. attracts a 10,000 Aro1914 creationRewardFeeToPay from the totalRewardAmount of 10,000,000 Aro1914s
+	// 2. attracts a 1,000 Aro1914 creationRewardFeeToPay from the totalRewardAmount of 1,000,000 Aro1914s
+	flatAlgoCreationFee: reach.parseCurrency(1001), // 1001 Algos
+	stakeToken,
+	rewardToken,
+	beginBlock: (await reach.getNetworkTime()).add(100), // 100 blocks from the point of creation
+	endBlock: (await reach.getNetworkTime()).add(200), // 100 blocks after the begin block begins, 200 blocks from the point of creation
+	totalRewardAmount: 1_000_000, // 1,000,000 Aro1914 tokens
+	totalAlgoRewardAmount: reach.parseCurrency(1_000_000), // 1,000,000 Algos
+	lockLengthBlocks: 50, // 1. 50 farm blocks from the point of staking, this leaves a window of 50 blocks for the staked tokens to attract claimable rewards,
+	// after which users can then decide to un-stake their stake tokens
+})
+
+export const params_2 = async (admin, stakeToken) => ({
+	beneficiary: admin.getAddress(),
+	creationFee: 10, // 0.1%,
+	// 1. attracts a 1,000 Algo creationAlgoFeeToPay from the totalAlgoRewardAmount of 1,000,000 Algos
+	// 2. attracts a 1,000 Aro1914 creationRewardFeeToPay from the totalRewardAmount of 1,000,000 Aro1914s
 	flatAlgoCreationFee: reach.parseCurrency(100), // 100 Algos
+	stakeToken,
+	rewardToken: stakeToken,
+	beginBlock: (await reach.getNetworkTime()).add(100), // 100 blocks from the point of creation
+	endBlock: (await reach.getNetworkTime()).add(200), // 100 blocks after the begin block begins, 200 blocks from the point of creation
+	totalRewardAmount: 1_000_000, // 1,000,000 Aro1914 tokens
+	totalAlgoRewardAmount: reach.parseCurrency(1_000_000), // 1,000,000 Algos
+	lockLengthBlocks: 50, // 1. 50 farm blocks from the point of staking, this leaves a window of 50 blocks for the staked tokens to attract claimable rewards,
+	// after which users can then decide to un-stake their stake tokens
+})
+
+export const params_3 = async (admin, stakeToken, rewardToken) => ({
+	beneficiary: admin.getAddress(),
+	creationFee: 2010, // 20.1%,
+	// 1. attracts a 201,000 Algo creationAlgoFeeToPay from the totalAlgoRewardAmount of 1,000,000 Algos
+	// 2. attracts a 201,000 Aro1914 creationRewardFeeToPay from the totalRewardAmount of 1,000,000 Aro1914s
+	flatAlgoCreationFee: reach.parseCurrency(100), // 100 Algos
+	stakeToken,
+	rewardToken,
+	beginBlock: (await reach.getNetworkTime()).add(100), // 300 blocks from the point of creation
+	endBlock: (await reach.getNetworkTime()).add(200), // 100 blocks after the begin block begins, 200 blocks from the point of creation
+	totalRewardAmount: 1_000_000, // 1,000,000 Aro1914 tokens
+	totalAlgoRewardAmount: reach.parseCurrency(1_000_000), // 1,000,000 Algos
+	lockLengthBlocks: 50, // 1. 50 farm blocks from the point of staking, this leaves a window of 50 blocks for the staked tokens to attract claimable rewards,
+	// after which users can then decide to un-stake their stake tokens
+})
+
+export const params_4 = async (admin, stakeToken, rewardToken) => ({
+	beneficiary: admin.getAddress(),
+	creationFee: 10, // 0.1%,
+	// 1. attracts a 1,000 Algo creationAlgoFeeToPay from the totalAlgoRewardAmount of 1,000,000 Algos
+	// 2. attracts a 1,000 Aro1914 creationRewardFeeToPay from the totalRewardAmount of 1,000,000 Aro1914s
+	flatAlgoCreationFee: reach.parseCurrency(100), // 100 Algos
+	stakeToken,
+	rewardToken,
+	beginBlock: (await reach.getNetworkTime()).add(300), // 300 blocks from the point of creation
+	endBlock: (await reach.getNetworkTime()).add(200), // 100 blocks after the begin block begins, 200 blocks from the point of creation
+	totalRewardAmount: 1_000_000, // 1,000,000 Aro1914 tokens
+	totalAlgoRewardAmount: reach.parseCurrency(1_000_000), // 1,000,000 Algos
+	lockLengthBlocks: 50, // 1. 50 farm blocks from the point of staking, this leaves a window of 50 blocks for the staked tokens to attract claimable rewards,
+	// after which users can then decide to un-stake their stake tokens
+})
+
+export const params_5 = async (admin, stakeToken, rewardToken) => ({
+	beneficiary: admin.getAddress(),
+	creationFee: 10, // 0.1%,
+	// 1. attracts a 1,000 Algo creationAlgoFeeToPay from the totalAlgoRewardAmount of 1,000,000 Algos
+	// 2. attracts a 1,000 Aro1914 creationRewardFeeToPay from the totalRewardAmount of 1,000,000 Aro1914s
+	flatAlgoCreationFee: reach.parseCurrency(100), // 100 Algos
+	stakeToken,
+	rewardToken,
+	beginBlock: (await reach.getNetworkTime()).add(100), // 100 blocks from the point of creation
+	endBlock: (await reach.getNetworkTime()).add(10000101), // 10000001 blocks after the begin block begins, 10000101 blocks from the point of creation
+	totalRewardAmount: 1_000_000, // 1,000,000 Aro1914 tokens
+	totalAlgoRewardAmount: reach.parseCurrency(1_000_000), // 1,000,000 Algos
+	lockLengthBlocks: 50, // 1. 50 farm blocks from the point of staking, this leaves a window of 50 blocks for the staked tokens to attract claimable rewards,
+	// after which users can then decide to un-stake their stake tokens
+})
+
+export const params_6 = async (admin, stakeToken, rewardToken) => ({
+	beneficiary: admin.getAddress(),
+	creationFee: 10, // 0.1%,
+	// 1. attracts a 1,000 Algo creationAlgoFeeToPay from the totalAlgoRewardAmount of 1,000,000 Algos
+	// 2. attracts a 1,000 Aro1914 creationRewardFeeToPay from the totalRewardAmount of 1,000,000 Aro1914s
+	flatAlgoCreationFee: reach.parseCurrency(100), // 100 Algos
+	stakeToken,
+	rewardToken,
+	beginBlock: (await reach.getNetworkTime()).add(100), // 100 blocks from the point of creation
+	endBlock: (await reach.getNetworkTime()).add(10000100), // 10000000 blocks after the begin block begins, 10000100 blocks from the point of creation
+	totalRewardAmount: 1_000_000, // 1,000,000 Aro1914 tokens
+	totalAlgoRewardAmount: reach.parseCurrency(1_000_000), // 1,000,000 Algos
+	lockLengthBlocks: 10000001, // 1. 10000001 farm blocks from the point of staking, this leaves a window of 10000001 blocks for the staked tokens to attract claimable rewards,
+	// after which users can then decide to un-stake their stake tokens
+});
+
+export const params_7 = async (admin, stakeToken, rewardToken) => ({
+	beneficiary: admin.getAddress(),
+	creationFee: 10, // 0.1%,
+	// 1. attracts a 1,000 Algo creationAlgoFeeToPay from the totalAlgoRewardAmount of 1,000,000 Algos
+	// 2. attracts a 1,000 Aro1914 creationRewardFeeToPay from the totalRewardAmount of 1,000,000 Aro1914s
+	flatAlgoCreationFee: reach.parseCurrency(100), // 100 Algos
+	stakeToken,
+	rewardToken,
+	beginBlock: (await reach.getNetworkTime()).add(100), // 100 blocks from the point of creation
+	endBlock: (await reach.getNetworkTime()).add(200), // 100 blocks after the begin block begins, 200 blocks from the point of creation
+	totalRewardAmount: 1_000_000, // 1,000,000 Aro1914 tokens
+	totalAlgoRewardAmount: reach.parseCurrency(1_000_000), // 1,000,000 Algos
+	lockLengthBlocks: 101, // 1. 101 farm blocks from the point of staking, this leaves a window of 101 blocks for the staked tokens to attract claimable rewards,
+	// after which users can then decide to un-stake their stake tokens
+});
+
+export const params_8 = async (admin, stakeToken, rewardToken) => ({
+	beneficiary: admin.getAddress(),
+	creationFee: 10, // 0.1%,
+	// 1. attracts a 1,000 Algo creationAlgoFeeToPay from the totalAlgoRewardAmount of 1,000,000 Algos
+	// 2. attracts a 1,000 Aro1914 creationRewardFeeToPay from the totalRewardAmount of 1,000,000 Aro1914s
+	flatAlgoCreationFee: reach.parseCurrency(100), // 100 Algos
+	stakeToken,
+	rewardToken,
+	beginBlock: (await reach.getNetworkTime()).add(100), // 100 blocks from the point of creation
+	endBlock: (await reach.getNetworkTime()).add(200), // 100 blocks after the begin block begins, 200 blocks from the point of creation
+	totalRewardAmount: 0, // 0 Aro1914 tokens
+	totalAlgoRewardAmount: reach.parseCurrency(1_000_000), // 1,000,000 Algos
+	lockLengthBlocks: 50, // 1. 50 farm blocks from the point of staking, this leaves a window of 50 blocks for the staked tokens to attract claimable rewards,
+	// after which users can then decide to un-stake their stake tokens
+});
+
+export const params_9 = async (admin, stakeToken, rewardToken) => ({
+	beneficiary: admin.getAddress(),
+	creationFee: 10, // 0.1%,
+	// 1. attracts a 1,000 Algo creationAlgoFeeToPay from the totalAlgoRewardAmount of 1,000,000 Algos
+	// 2. attracts a 1,000 Aro1914 creationRewardFeeToPay from the totalRewardAmount of 1,000,000 Aro1914s
+	flatAlgoCreationFee: reach.parseCurrency(1001), // 1001 Algos
 	stakeToken,
 	rewardToken,
 	beginBlock: (await reach.getNetworkTime()).add(100), // 100 blocks from the point of creation
--- index.test.mjs
+++ index.test.mjs
@@ -9,6 +9,7 @@ import {
 	b2N,
 } from './index.func.0.js'
 import * as f1 from './index.func.1.js'
+import * as f2 from './index.func.2.js'
 const users = ['admin', 'creator', 'user', 'tAcc1', 'tAcc2', 'tAcc3', 'tAcc4']
 
 test.one('Users can stake, claim, and unstake', async () => {
@@ -66,6 +67,244 @@ test.one('Users can stake, claim, and unstake', async () => {
 				},
 			})
 		),
+	])
+})
+
+test.one('Flat ALGO creation fee cannot be more than 1000 ALGO', async () => {
+	const { admin, creator, stakeToken, rewardToken } = await f2.setup()
+	const ctc = deploy(creator)
+
+	return await test.chkErr(
+		'Params with flatAlgoCreationFee of 1001 throws a check failure',
+		'Flat ALGO creation fee cannot be more than 1000 ALGO',
+		async () => {
+			await ctc.p.Creator({
+				getParams: async () =>
+					await f2.params_1(admin, stakeToken, rewardToken),
+				deployed: () =>
+					reach.withDisconnect(() => {
+						console.log('[+] admin (beneficiary) saw deploy confirmed')
+						reach.disconnect(null)
+					}),
+			})
+		}
+	)
+})
+
+test.one(
+	'Reach requires all tokens to be different. Consider using distribution contract instead.',
+	async () => {
+		const { admin, creator, stakeToken } = await f2.setup()
+		const ctc = deploy(creator)
+
+		return await test.chkErr(
+			'Params with the same tokenID for reward and stakeToken throws a check failure',
+			'Reach requires all tokens to be different. Consider using distribution contract instead.',
+			async () => {
+				await ctc.p.Creator({
+					getParams: async () => await f2.params_2(admin, stakeToken),
+					deployed: () =>
+						reach.withDisconnect(() => {
+							console.log('[+] admin (beneficiary) saw deploy confirmed')
+							reach.disconnect(null)
+						}),
+				})
+			}
+		)
+	}
+)
+
+test.one('Maximum possible creation fee is 20%', async () => {
+	const { admin, creator, stakeToken, rewardToken } = await f2.setup()
+	const ctc = deploy(creator)
+
+	return await test.chkErr(
+		'Params with a creation fee greater than 20% throws a check failure',
+		'Maximum possible creation fee is 20%',
+		async () => {
+			await ctc.p.Creator({
+				getParams: async () =>
+					await f2.params_3(admin, stakeToken, rewardToken),
+				deployed: () =>
+					reach.withDisconnect(() => {
+						console.log('[+] admin (beneficiary) saw deploy confirmed')
+						reach.disconnect(null)
+					}),
+			})
+		}
+	)
+})
+
+test.one('End block must be greater than begin block', async () => {
+	const { admin, creator, stakeToken, rewardToken } = await f2.setup()
+	const ctc = deploy(creator)
+
+	return await test.chkErr(
+		'Params with a begin block greater than the end block throws a check failure',
+		'',
+		async () => {
+			await ctc.p.Creator({
+				getParams: async () =>
+					await f2.params_4(admin, stakeToken, rewardToken),
+				deployed: () =>
+					reach.withDisconnect(() => {
+						console.log('[+] admin (beneficiary) saw deploy confirmed')
+						reach.disconnect(null)
+					}),
+			})
+		}
+	)
+})
+
+test.one(
+	'End block minus begin block must be less than 10000000 blocks',
+	async () => {
+		const { admin, creator, stakeToken, rewardToken } = await f2.setup()
+		const ctc = deploy(creator)
+
+		return await test.chkErr(
+			'Params with a difference between the end block and the begin block greater than 10000000 blocks throws a check failure',
+			'',
+			async () => {
+				await ctc.p.Creator({
+					getParams: async () =>
+						await f2.params_5(admin, stakeToken, rewardToken),
+					deployed: () =>
+						reach.withDisconnect(() => {
+							console.log('[+] admin (beneficiary) saw deploy confirmed')
+							reach.disconnect(null)
+						}),
+				})
+			}
+		)
+	}
+)
+
+test.one(
+	'lockLenghtBlocks block must be less than 10000000 blocks',
+	async () => {
+		const { admin, creator, stakeToken, rewardToken } = await f2.setup()
+		const ctc = deploy(creator)
+
+		return await test.chkErr(
+			'Params with a lockLenghtBlocks greater than 10000000 blocks throws a check failure',
+			'',
+			async () => {
+				await ctc.p.Creator({
+					getParams: async () =>
+						await f2.params_6(admin, stakeToken, rewardToken),
+					deployed: () =>
+						reach.withDisconnect(() => {
+							console.log('[+] admin (beneficiary) saw deploy confirmed')
+							reach.disconnect(null)
+						}),
+				})
+			}
+		)
+	}
+)
+
+test.one(
+	'lockLenghtBlocks block must be <= end block - begin block',
+	async () => {
+		const { admin, creator, stakeToken, rewardToken } = await f2.setup()
+		const ctc = deploy(creator)
+
+		return await test.chkErr(
+			'Params with a lockLenghtBlocks > end block - begin block throws a check failure',
+			'',
+			async () => {
+				await ctc.p.Creator({
+					getParams: async () =>
+						await f2.params_7(admin, stakeToken, rewardToken),
+					deployed: () =>
+						reach.withDisconnect(() => {
+							console.log('[+] admin (beneficiary) saw deploy confirmed')
+							reach.disconnect(null)
+						}),
+				})
+			}
+		)
+	}
+)
+
+test.one('totalRewardAmount must be > 0', async () => {
+	const { admin, creator, stakeToken, rewardToken } = await f2.setup()
+	const ctc = deploy(creator)
+
+	return await test.chkErr(
+		'Params with a totalRewardAmount <= 0 throws a check failure',
+		'',
+		async () => {
+			await ctc.p.Creator({
+				getParams: async () =>
+					await f2.params_8(admin, stakeToken, rewardToken),
+				deployed: () =>
+					reach.withDisconnect(() => {
+						console.log('[+] admin (beneficiary) saw deploy confirmed')
+						reach.disconnect(null)
+					}),
+			})
+		}
+	)
+})
+
+test.one('flatAlgoCreationFee must be <= 1000 Algo', async () => {
+	const { admin, creator, stakeToken, rewardToken } = await f2.setup()
+	const ctc = deploy(creator)
+
+	return await test.chkErr(
+		'Params with a flatAlgoCreationFee > 1000 Algo throws a check failure',
+		'',
+		async () => {
+			await ctc.p.Creator({
+				getParams: async () =>
+					await f2.params_9(admin, stakeToken, rewardToken),
+				deployed: () =>
+					reach.withDisconnect(() => {
+						console.log('[+] admin (beneficiary) saw deploy confirmed')
+						reach.disconnect(null)
+					}),
+			})
+		}
+	)
+})
+
+test.one('Only the beneficiary address is allowed to claimFees', async () => {
+	const { admin, creator, stakeToken, user, rewardToken } = await f2.setup()
+	const ctc = deploy(creator)
+
+	const info = ctc.getInfo()
+	const ctcUser = user.contract(backend, info)
+	console.log('[+] attached user to the main contract')
+
+	return await Promise.all([
+		reach.withDisconnect(() => {
+			ctc.p.Creator({
+				getParams: async () => await f1.params(admin, stakeToken, rewardToken),
+				deployed: async () => {
+					console.log('[+] admin (beneficiary) saw deploy confirmed')
+					const info = await ctc.getInfo()
+					const ctcCreator = creator.contract(backend, info)
+					await test.chkErr(
+						'Only the beneficiary address is allowed to claimFees',
+						'only beneficiary can claim creation fees',
+						async () => {
+							await ctcCreator.apis.claimFees()
+						}
+					)
+					reach.disconnect(null)
+				},
+			})
+		}),
+		reach.withDisconnect(() => {
+			ctcUser.p.User({
+				deployed: () => {
+					console.log('[+] user saw deploy confirmed')
+					reach.disconnect(null)
+				},
+			})
+		}),
 	])
 })
 
